83e9dd7646d0f4daddfbff1c28020512010bfef5
bryamphan <bryamphan@csu.fullerton.edu>
2025-12-04 01:21:32 -0800

    Created a template to start on my project.

---
+++ app.js
@@ -0,0 +1,18 @@
+// importing react hooks
+const { useState, useEffect } = React;
+
+// Main App Component
+function App() {
+    // state variables here todo
+    
+    return (
+        <div>
+            <h1>FlowGrind</h1>
+            {/* build app here */}
+        </div>
+    );
+}
+
+// app render
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
